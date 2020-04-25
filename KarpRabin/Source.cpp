41b32b552416a8954a3d18f727d2ca060c3d1795
Michał Żuchowski <michal.zuchowski@unilodz.eu>
2020-04-25 22:53:02 +0200

    basic from Cormen seems working for bibliaEn

--- KarpRabin/Source.cpp
+++ KarpRabin/Source.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 #include<fstream>
+#include<cmath>
+#include<string>
 
-int hashfun(std::string s)
-{
-	int hash = 0;
-	for (int i = 0; i < s.length(); i++)
-	{
-		hash += s[i];
-	}
-	return hash;
-}
+//int hashfun(std::string s)
+//{
+//	int hash = 0;
+//	for (int i = 0; i < s.length(); i++)
+//	{
+//		hash += s[i]
+//	}
+//	return hash;
+//}
 
 
 int main()
 {
-	std::fstream plik;
-	plik.open("test.txt");
+	/*std::fstream plik;
+	plik.open("bibliaPL.txt");
 
 	if (plik.good())
 	{
@@ -26,17 +28,18 @@ int main()
 	std::cin >> szukane;
 	
 	int liczbaznakow = 0;
-	while (!plik.eof())
+	char c;
+	while (plik >> c)
 	{
-		plik.get();
 		liczbaznakow++;
 	}
+	liczbaznakow++;
+	std::cout << "liczba znakow: " << liczbaznakow << std::endl;
 	plik.close();
-	plik.open("test.txt");
+	plik.open("bibliaPL.txt");
 
 
 	std::string s;
-	std::string check;
 	std::string temp;
 
 	for (int i = 0; i < liczbaznakow - 1; i++)
@@ -49,24 +52,82 @@ int main()
 
 		if (hashfun(szukane) == hashfun(s))
 		{
-			
-			
 			if (s == szukane)
 			{
 				std::cout << i << " ";
-				
 			}
-			
-			
 		}
-		
 		plik.seekg(i + 1, std::ios::beg);
 		s.clear();
-		check.clear();
 		
+	}
+	plik.close();*/
+	
+	const int q = 127;
+	const int d = 127;
+	
+	std::fstream plik;
+	plik.open("bibliaEn.txt");
+
+	std::string szukane;
+	//std::cin >> szukane;
+
+	std::getline(std::cin, szukane);
+
+	int n=0, m=0;
+	
+	m = szukane.size();
+
+	char temp;
+	while (!plik.eof())
+	{
+		plik.get();
+		n++;//wykonac test czy dobrze liczy
+	}
 		
+
+	//n++;
+	plik.close();
+	plik.open("bibliaEn.txt");
+
+	int h = ((int)std::pow(d, (m - 1))) % q;
+
+	int p = 0, t0 = 0;
+	std::string tempString;
+
+	for (int i = 0; i < m; i++)
+	{
+		p = (((d * p) + szukane[i]) % q);
+		temp = plik.get();
+		tempString.push_back(temp);
+		t0 = (((d * t0) + temp) % q);
+	}
+	
+	int missed = 0;
+	for (int s = 0; s < n - m; s++)
+	{
+		if (p == t0)
+		{
+			if (szukane == tempString)
+			{
+				std::cout << s << " ";
+			}
+			else
+				missed++;
+
+		}
+		if (s < n - m)
+		{
+			temp = plik.get();
+			tempString.push_back(temp);
+			t0 = (((d * (t0 - (tempString[0] * h))) + temp) % q); 
+			tempString = tempString.substr(1);
+		}
 	}
 	plik.close();
 	
+	std::cout << std::endl << missed;
+	
+
 	return 0;
 }
