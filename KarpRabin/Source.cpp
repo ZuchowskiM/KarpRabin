26fe21fa99f663d4d368fda6ca458b00165e8ce0
Michał Żuchowski <michal.zuchowski@unilodz.eu>
2020-04-30 23:27:27 +0200

    Zastosowano rozwiąznie Kowalczyka, wydaje sie dzialac nawet dla polskich znakow, basic

--- KarpRabin/Source.cpp
+++ KarpRabin/Source.cpp
@@ -4,69 +4,14 @@
 #include<string>
 #include<windows.h>
 
-//int hashfun(std::string s)
-//{
-//	int hash = 0;
-//	for (int i = 0; i < s.length(); i++)
-//	{
-//		hash += s[i]
-//	}
-//	return hash;
-//}
-
 
 int main()
 {
-	/*std::fstream plik;
-	plik.open("bibliaPL.txt");
-
-	if (plik.good())
-	{
-		std::cout << "otwarto" << std::endl;
-	}
-
-	std::string szukane;
-	std::cin >> szukane;
 	
-	int liczbaznakow = 0;
-	char c;
-	while (plik >> c)
-	{
-		liczbaznakow++;
-	}
-	liczbaznakow++;
-	std::cout << "liczba znakow: " << liczbaznakow << std::endl;
-	plik.close();
-	plik.open("bibliaPL.txt");
-
-
-	std::string s;
-	std::string temp;
-
-	for (int i = 0; i < liczbaznakow - 1; i++)
-	{
-		for (int j = 0;j < szukane.size(); j++)
-		{
-			temp = plik.get();
-			s.append(temp);
-		}
-
-		if (hashfun(szukane) == hashfun(s))
-		{
-			if (s == szukane)
-			{
-				std::cout << i << " ";
-			}
-		}
-		plik.seekg(i + 1, std::ios::beg);
-		s.clear();
-		
-	}
-	plik.close();*/
 	SetConsoleCP(1250);
 
-	const int q = 260144641;
-	const int d = 127;
+	const int q = 1801;
+	const int d = 256;
 	
 	std::fstream plik;
 	plik.open("bibliaPL.txt");
@@ -83,29 +28,30 @@ int main()
 	{
 		plik.get();
 		n++;//liczy o jeden wiecej ale to dobrze
-	}
-	//n--;
-	//std::cout << "liczba znakow w pliku: " << n << std::endl;
-
-
+	}//zrobic test czy jesli ostani ciag jest w ostani w tekscie to poprawnie wyszuka
+	
 	plik.close();
 	plik.open("bibliaPL.txt");
 
-	unsigned long long h = ((int)std::pow(d, (m - 1)));//% q;
+	int h = d;
 
-	unsigned long long p = 0, t0 = 0;
+	for (int i = 0; i < m - 2; i++)
+	{
+		h = (h * d) % q;
+	}
+
+	int p = 0, t0 = 0;
 	std::string tempString;
 
 	for (int i = 0; i < m; i++)
 	{
-		p = (((d * p) + szukane[i]) % q);
-		//p = ((d * p) + szukane[i]);
-
+		p = (((d * p) % q) + szukane[i]) % q;
+		
 		temp = plik.get();
 		tempString.push_back(temp);
 
-		t0 = (((d * t0) + temp) % q);
-		//t0 = ((d * t0) + temp);
+		t0 = (((d * t0) % q) + temp) % q;
+		
 	}
 	
 	int missed = 0, found = 0;
@@ -126,9 +72,9 @@ int main()
 		{
 			temp = plik.get();
 			tempString.push_back(temp);
-			//t0 = ((d * (t0 - (tempString[0] * h))) + temp);
-			//t0 = ((d * t0  + temp) % q);
-			t0 = ((d * (t0 % h)) + temp) % q;
+
+			t0 = ((d * (((t0 - ((tempString[0] * h) % q)) + q) % q) % q) + temp) % q;
+			
 			tempString = tempString.substr(1);
 		}
 	}
